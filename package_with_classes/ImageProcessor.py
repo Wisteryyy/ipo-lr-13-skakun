abde297137fe171dda770115f0b1b685ab0b3690
Sofia <sofiaskakun30@gmail.com>
2024-12-19 00:02:32 +0300

    ImageProcessor.py

--- package_with_classes/ImageProcessor.py
+++ package_with_classes/ImageProcessor.py
@@ -1,3 +1,4 @@
+# ImageProcessor — класс для обработки изображения (применение фильтров, добавление текста, поворот).
 from PIL import ImageFilter, ImageDraw, ImageFont
 
 class ImageProcessor:
@@ -9,21 +10,25 @@ class ImageProcessor:
             if filter_type == 'BLUR':
                 self.image = self.image.filter(ImageFilter.BLUR)
                 print("Применен фильтр размытия.")
+            elif filter_type == 'CONTOUR':
+                self.image = self.image.filter(ImageFilter.CONTOUR)
+                print("Применен контурный фильтр.")
         else:
             print("Изображение не загружено.")
 
-    def add_text(self, text, position=(10, 10), font_size=20):
+    def add_text(self, text, position=None, font_size=20):
         if self.image:
             draw = ImageDraw.Draw(self.image)
             font = ImageFont.load_default()
+            
+            text_bbox = draw.textbbox((0, 0), text, font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+
+            if position is None:
+                position = (self.image.width - text_width - 10, self.image.height - text_height - 10)
+
             draw.text(position, text, fill="white", font=font)
             print("Текст добавлен на изображение.")
-        else:
-            print("Изображение не загружено.")
-
-    def rotate_image(self, angle):
-        if self.image:
-            self.image = self.image.rotate(angle)
-            print(f"Изображение повернуто на {angle} градусов.")
         else:
             print("Изображение не загружено.")
